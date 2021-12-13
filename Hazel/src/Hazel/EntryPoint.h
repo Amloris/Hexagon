45a69e9066d11b11738768be8ddb2af8e7efa455
Amloris <robertson.aarona@gmail.com>
2021-12-12 21:43:07 -0600

    Setup basic Application and Entry Point

---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Application.h"
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** argv)
+{
+	printf("Hazel Engine");
+	auto app = Hazel::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+#endif
